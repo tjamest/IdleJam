ef09c743f02a8a109a4b1e09a271124f1e562ea6
Michelle Santiago Beadle <michelle.santiago@fortisgames.com>
2024-06-22 16:39:43 -0300

    resource button

---
+++ CrowJam/Assets/GameManager.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject TextParent;
+    public GameObject TextPrefab;
+
+    public List<Button> buttons = new List<Button>();
+    public List<ResourceScriptableObject> resources = new List<ResourceScriptableObject>();
+
+    protected List<Tuple<GameObject, ResourceType>> resourceText = new List<Tuple<GameObject, ResourceType>>();
+
+    public void Start()
+    {
+    }
+
+    public void Update()
+    {
+        foreach (var resource in resources)
+        {
+
+        }
+
+    }
+
+    // Button Resource
+    public void AddMoneyResource(float value)
+    {
+        AddResource(ResourceType.Money, value);
+    }
+
+    public void AddOtherResource(float value)
+    {
+        AddResource(ResourceType.Other, value);
+    }
+
+    protected void AddResource(ResourceType type, float value)
+    {
+        ResourceScriptableObject ResourceSO = GetResource(type);
+        ResourceSO.amount += value;
+
+        if (!HasResourceText(type))
+        {
+            TextMeshProUGUI[] textChildren = TextPrefab.GetComponentsInChildren<TextMeshProUGUI>();
+            textChildren[0].text = ResourceSO.type.ToString();
+            textChildren[1].text = ResourceSO.amount.ToString();
+
+            resourceText.Add(new Tuple<GameObject, ResourceType>(Instantiate(TextPrefab, TextParent.transform), type));
+        }
+        else
+        {
+            UpdateResourceText(type, ResourceSO.amount);
+        }
+    }
+
+    protected void UpdateResourceText(ResourceType type, float amount)
+    {
+        foreach (var item in resourceText)
+        {
+            if (item.Item2 == type)
+            {
+                TextMeshProUGUI[] textChildren = item.Item1.GetComponentsInChildren<TextMeshProUGUI>();
+                textChildren[1].text = amount.ToString();
+            }
+        }
+    }
+
+    protected bool HasResourceText(ResourceType type)
+    {
+        if (resourceText.Count > 0)
+        {
+            foreach (var item in resourceText)
+            {
+                if (item.Item2 == type)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    protected ResourceScriptableObject GetResource(ResourceType type)
+    {
+        foreach (var resource in resources)
+        {
+            if (resource.type == type)
+            {
+                return resource;
+            }
+        }
+
+        return null;
+    }
+
+
+}
---
+++ CrowJam/Assets/Script/ResourceScriptableObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Resources", order = 1)]
+public class ResourceScriptableObject : ScriptableObject
+{
+    public ResourceType type;
+    public float amount;
+
+    public bool automated;
+    public float cycleDuration;
+    public float increase;
+    public float multiplier;
+}
+
+public enum ResourceType
+{
+    Money,
+    Other
+}
